a395e96b2ef38bc9dcdada800b539949f055c816
xxcj <xxcj-sh@outlook.com>
2025-10-04 02:40:52 +0800

    Update

--- app/utils/db_init.py
+++ app/utils/db_init.py
@@ -2,6 +2,9 @@ from app.utils.db_config import Base, engine
 from app.models import User, Match, MatchDetail
 from app.models.match_action import MatchAction, MatchResult
 from app.models.user_profile import UserProfile
+from app.models.user_card_db import UserCard
+from app.models.llm_usage_log import LLMUsageLog
+from app.models.order import MembershipOrder
 
 def init_db():
     """初始化数据库，创建所有表"""
