e001871f1653967fd6b8ebcd79f102711b21eb9f
xxcj-sh <103134986+xxcj-sh@users.noreply.github.com>
2026-02-10 23:18:04 +0800

    feat: 实现头像持久化存储功能

--- sql/migrate_add_birthday.sql
+++
@@ -1,9 +0,0 @@
--- Migration script to add birthday column to users table
--- Run this SQL in your database management tool (MySQL)
-
-ALTER TABLE users ADD COLUMN birthday VARCHAR(20) DEFAULT NULL COMMENT '用户出生日期';
-
--- Verify the column was added
--- SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_COMMENT
--- FROM INFORMATION_SCHEMA.COLUMNS
--- WHERE TABLE_NAME = 'users' AND TABLE_SCHEMA = 'your_database_name';
